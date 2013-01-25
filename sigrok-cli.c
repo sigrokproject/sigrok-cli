2777cbd2d252c1ef23e112e1771052a41aa588eb
Bert Vermeulen <bert@biot.com>
2013-01-25 11:00:53 +0100

    Deprecate SR_DI_BUFFERSIZES.

--- sigrok-cli.c
+++ sigrok-cli.c
@@ -412,7 +412,7 @@ static void show_dev_detail(void)
 		} else if (srci->key == SR_CONF_BUFFERSIZE) {
 			/* Supported buffer sizes */
 			printf("    %s", srci->id);
-			if (sr_info_get(sdi->driver, SR_DI_BUFFERSIZES,
+			if (sr_config_list(sdi->driver, SR_CONF_BUFFERSIZE,
 					(const void **)&integers, sdi) != SR_OK) {
 				printf("\n");
 				continue;
